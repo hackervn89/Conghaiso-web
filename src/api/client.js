2d210ea9650401f73ddefe20efa1504c37846385
hackernv89 <hackervn89@gmail.com>
2025-09-15 17:03:22 +0700

    Hoàn thành 15-09

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api', // Địa chỉ của server backend
+  baseURL: 'https://conghaiso-backend.onrender.com/api', // Địa chỉ của server backend
   headers: {
     'Content-Type': 'application/json',
   },
