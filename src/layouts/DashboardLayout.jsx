596b6ce516e3c3a15b01b2ceb7c13edb14779c40
hackernv89 <hackervn89@gmail.com>
2025-09-17 16:54:12 +0700

    PWS1

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -93,9 +93,12 @@ const DashboardLayout = () => {
                         <span className="mr-4 text-gray-700">Chào đồng chí, <span className="font-semibold text-primaryRed">{user?.fullName}</span></span>
                         <button
                             onClick={signOut}
-                            className="px-4 py-2 font-bold text-white bg-primaryRed rounded-md hover:bg-red-700"
+                            className="p-2 md:px-4 md:py-2 font-bold text-white bg-primaryRed rounded-md hover:bg-red-700 flex items-center gap-2"
                         >
-                            Đăng xuất
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
+                            <span className="hidden md:inline">Đăng xuất</span>
                         </button>
                     </div>
                 </header>
